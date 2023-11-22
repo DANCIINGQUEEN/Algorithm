0a2f23d26cef9a9e160c117ababb33c8bb69f4fb
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2023-11-22 20:20:56 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.55 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,10 +1,19 @@
-function solution(s) {
-    let firstArr=[]
-    let nearArr=[]
-    s.split``.forEach((x, i)=>{
-        if(firstArr.includes(x))  nearArr.push(i-firstArr.lastIndexOf(x))
-        else nearArr.push(-1)
-        firstArr.push(x)
-    })
-    return nearArr
+// function solution(s) {
+//     let firstArr=[]
+//     let nearArr=[]
+//     s.split``.forEach((x, i)=>{
+//         if(firstArr.includes(x))  nearArr.push(i-firstArr.lastIndexOf(x))
+//         else nearArr.push(-1)
+//         firstArr.push(x)
+//     })
+//     return nearArr
+// }
+function solution(s){
+      const hash={};
+
+    return [...s].map((v,i)=>{
+        let result = hash[v] !== undefined ? i - hash[v] : -1;
+        hash[v] = i;
+        return result;
+    });
 }
