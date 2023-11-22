70ecc34dfae0f9c32383ba68a8499cd40a0ee576
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2023-11-22 20:17:54 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 6.51 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,22 +1,10 @@
 function solution(s) {
     let firstArr=[]
     let nearArr=[]
-    // console.log(s.split``)
     s.split``.forEach((x, i)=>{
-        // console.log(firstArr, 'firstArr')
-        // console.log(nearArr, 'nearArr')
-        if(firstArr.includes(x)) {
-            // console.log(x, '2', firstArr.includes(x), '--------------------')
-            // console.log(i, firstArr.lastIndexOf(x))
-            nearArr.push(i-firstArr.lastIndexOf(x))
-        }
-        else{
-            // console.log(x, -1, firstArr.includes(x), '--------------------')
-            nearArr.push(-1)
-        }
+        if(firstArr.includes(x))  nearArr.push(i-firstArr.lastIndexOf(x))
+        else nearArr.push(-1)
         firstArr.push(x)
     })
-    // console.log(nearArr)
-    
     return nearArr
 }
