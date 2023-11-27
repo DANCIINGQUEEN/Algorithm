d444d668179ba1721843ff87a1d36f0449fe043e
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2023-11-27 20:42:20 +0900

    [level 2] Title: 의상, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,14 +1,3 @@
 function solution(clothes) {
-    // console.log([...new Set(clothes[])])
-    // console.log([...new Set((clothes.map((x,i)=>x=x[1])))])
-    let a={}
-    clothes.forEach((x,i)=>{
-        a[x[1]]=++a[x[1]]||1
-        console.log(x)
-    })
-    console.log(a)
-    // console.log(Object.keys(a).length)
-    // console.log(Object.values(a).reduce((a,c)=>a*c,1))
-    
-    return Object.keys(a).length===1?clothes.length:Object.values(a).reduce((a,c)=>a*(c+1),1)-1
+    return Object.values(clothes.reduce((a,c)=>(a[c[1]]=++a[c[1]]||1,a),{})).reduce((a,c)=>a*(c+1),1)-1
 }
