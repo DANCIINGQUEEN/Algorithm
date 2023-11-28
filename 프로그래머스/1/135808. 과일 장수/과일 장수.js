2020bdda5a37c095eca7296fe9fbd65321a0b124
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2023-11-28 10:08:44 +0900

    [level 1] Title: 과일 장수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,9 +1,6 @@
 function solution(k, m, score) {
     score.sort((a,b)=>b-a)
-    let s=0
-    for(let i=1;i<=~~(score.length/m);i++){
-        s+=score[m*i-1]*m
-    }
+    for(var i=1, s=0;i<=~~(score.length/m);i++) s+=score[m*i-1]*m
     return s
     
 }
