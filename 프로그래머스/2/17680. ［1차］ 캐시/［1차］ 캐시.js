4ae5d213f84c2a9efbfe3420f55f7a41a65c51ed
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2023-11-30 13:26:59 +0900

    [level 2] Title: [1차] 캐시, Time: 0.59 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,7 +1,6 @@
 const solution=(cacheSize, cities)=>{
     cities=cities.map(x=>x.toUpperCase())   //cities 배열을 모두 대문자로
-    let a=0
-    let q=[]
+    let a=0, q=[]
     for (let c of cities){  
         if(!q.includes(c)){  //q에 cities가 없으면
             a+=5
@@ -15,18 +14,5 @@ const solution=(cacheSize, cities)=>{
             q.push(c)
         }
     }
-    // for(let i=0;i<cities.length;i++){
-    //     if(!q.includes(cities[i])){
-    //         a+=5
-    //         q.push(cities[i])
-    //         if(q.length>cacheSize) q.shift()
-    //     }
-    //     else{
-    //         a++
-    //         let idx=q.indexOf(cities[i])
-    //         q.splice(idx, i)
-    //         q.push(cities[i])
-    //     }
-    // }
     return a    
 }
