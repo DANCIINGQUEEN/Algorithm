afaf524586e2ab2821259a6b9d756807e1170a19
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2023-12-01 16:03:43 +0900

    [level 2] Title: 튜플, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -2,8 +2,7 @@ function solution(s) {
   const a=[]
   s.slice(2,s.length-2)
       .split`},{`
-      .map(s=>s.split`,`
-      .map(x=>+x))
+      .map(s=>s.split`,`.map(x=>+x))
       .sort((a,b)=>a.length-b.length)
       .forEach(r=>r.forEach(v=>!a.includes(v)&&a.push(v)))
   return a
