632e618a9a10e26ce8c43131ba26b6e8ab41155f
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2023-12-01 10:33:45 +0900

    [level 1] Title: 모의고사, Time: 2.55 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,12 @@
+function solution(answers) {
+    let s=[[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]
+    let ss=s.map((x,i)=>x=answers.filter((y,j)=>y===s[i][j%s[i].length]).length)
+    let a=[], max=Math.max(...ss)
+    ss.forEach((_,i)=>(ss[i]===max)&&a.push(i+1))
+    return a
+    
+
+    
+    
+    
+}
