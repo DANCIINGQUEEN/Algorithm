cee5d7c91a3b0c24e271e20c9f255cf5ac52afb7
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2023-12-01 18:51:24 +0900

    [level 1] Title: 덧칠하기, Time: 1.70 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,10 +1,11 @@
-function solution(n, m, section) {
-    let a=0, p=0
-    for(let i of section){
-        if(p<i){
-            a++
-            p=i+m-1
-        }
-    }
-    return a
-}
+// function solution(n, m, section) {
+//     let a=0, p=0
+//     for(let i of section){
+//         if(p<i){
+//             a++
+//             p=i+m-1
+//         }
+//     }
+//     return a
+// }
+const solution=(n,m,section)=>section.reduce((a,i)=>{return(a.p<i)?{a:a.a+1,p:i+m-1}:a},{a:0,p:0}).a
