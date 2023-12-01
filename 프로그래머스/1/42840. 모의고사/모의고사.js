ad4f21e3f5dad7c923d286963889215db00cf9c0
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2023-12-01 20:34:06 +0900

    [level 1] Title: 모의고사, Time: 4.89 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,5 +1,16 @@
-function solution(answers) {
+// function solution(answers) {
+//     let s=[[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]
+//     let max=Math.max(...(s=s.map((x,i)=>answers.filter((y,j)=>y===s[i][j%s[i].length]).length)))
+//     return s.reduce((a,v,i)=>(v===max?[...a,i+1]:a),[]);
+// }
+
+// const solution = (answers) => (s=[[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]], max=Math.max(...(s=s.map((x,i)=>answers.filter((y,j)=>y===s[i][j%s[i].length]).length)))) => s.reduce((a,v,i)=>(v===max?[...a,i+1]:a),[]);
+
+const solution=answers=>{
     let s=[[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]
-    let max=Math.max(...(s=s.map((x,i)=>answers.filter((y,j)=>y===s[i][j%s[i].length]).length)))
-    return s.reduce((a,v,i)=>(v===max?[...a,i+1]:a),[]);
+    let ss=s.map((x,i)=>answers.filter((y,j)=>y===s[i][j%s[i].length]).length)
+    let max=Math.max(...ss)
+    let a=[]
+    ss.forEach((_,i)=>ss[i]===max&&a.push(i+1))
+    return a
 }
