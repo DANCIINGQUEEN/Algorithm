a091b0fe51aaf41f72c3ace3893358623e96d82e
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2023-12-01 18:48:54 +0900

    [level 2] Title: 튜플, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,9 +1,9 @@
-function solution(s) {
-  const a=[]
-  s.slice(2,s.length-2)
-      .split`},{`
-      .map(s=>s.split`,`.map(x=>+x))
-      .sort((a,b)=>a.length-b.length)
-      .forEach(r=>r.forEach(v=>!a.includes(v)&&a.push(v)))
-  return a
+const solution=s=>{
+    let a=[]
+    s.slice(2, s.length-2)  //문자열 양 끝 제거
+        .split`},{` //},{ 기준으로 
+        .map(s=>s.split`,`.map(x=>+x))  //2차원배열 , 기준으로 자르고 Number로 형변환
+        .sort((a,b)=>a.length-b.length) //배열길이 오름차순 정렬
+        .forEach(x=>x.forEach(v=>!a.includes(v)&&a.push(v)))    // a배열에 v값이 없으면 a배열에v값 push
+    return a
 }
