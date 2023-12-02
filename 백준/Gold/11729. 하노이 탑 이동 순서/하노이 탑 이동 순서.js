ea0cb0888d35b35f2d4d89e1511ddf6aea849a3a
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2023-12-02 15:48:24 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 660 ms, Memory: 88952 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.js
@@ -0,0 +1,12 @@
+let n=+require('fs').readFileSync('dev/stdin').toString().trim();
+
+const h = (n, a, b) => {
+  if(n===1){
+    return `${a} ${b}`
+  }
+  else {
+    return `${h(n-1, a, 6-a-b)}\n${h(1, a, b)}\n${h(n-1, 6-a-b, b)}`;
+  }
+}
+console.log(2**n - 1);
+console.log(h(n,1,3));
