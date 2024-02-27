6f37e20f44c6f08400edf90df11112463f414281
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-02-27 19:07:45 +0900

    [level 2] Title: 타겟 넘버, Time: 1.09 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,13 +1,27 @@
-function solution(numbers, target) {
+// function solution(numbers, target) {
+//     let a=0
+//     dfs(0,0)
+//     function dfs(i, s){
+//         if(i===numbers.length){
+//             if(s===target) a++
+//             return
+//         }
+//         dfs(i+1, s+numbers[i])
+//         dfs(i+1, s-numbers[i])
+//     }
+//     return a
+// }
+
+function solution(n, t){
     let a=0
-    dfs(0,0)
+    dfs(0, 0)
     function dfs(i, s){
-        if(i===numbers.length){
-            if(s===target) a++
+        if(i===n.length){
+            if(s===t) a++
             return
         }
-        dfs(i+1, s+numbers[i])
-        dfs(i+1, s-numbers[i])
+        dfs(i+1, s+n[i])
+        dfs(i+1, s-n[i])
     }
     return a
 }
