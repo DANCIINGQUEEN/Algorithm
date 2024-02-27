0b12bba771e87f65452d17782804ce5ac92a0050
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-02-27 17:35:04 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -0,0 +1,19 @@
+function solution(n, computers) {
+  let a = 0
+  const v = Array(n).fill(false)
+  function dfs(index) {
+    v[index] = true
+    for (let i=0;i<n;i++) {
+      if (computers[index][i] && !v[i]) {
+        dfs(i)
+      }
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    if (!v[i]) {
+      dfs(i)
+      a++
+    }
+  }
+  return a
+}
