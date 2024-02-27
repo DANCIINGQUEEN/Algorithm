e587a9b67f284118f6eb325c711d1da0165b95df
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-02-27 18:53:48 +0900

    [level 3] Title: 네트워크, Time: 0.31 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,19 +1,38 @@
-function solution(n, computers) {
-  let a = 0
-  const v = Array(n).fill(false)
-  function dfs(index) {
-    v[index] = true
-    for (let i=0;i<n;i++) {
-      if (computers[index][i] && !v[i]) {
-        dfs(i)
-      }
+// function solution(n, computers) {
+//   let a = 0
+//   const v = Array(n).fill(false)
+//   function dfs(index) {
+//     v[index] = true
+//     for (let i=0;i<n;i++) {
+//       if (computers[index][i] && !v[i]) {
+//         dfs(i)
+//       }
+//     }
+//   }
+//   for (let i = 0; i < n; i++) {
+//     if (!v[i]) {
+//       dfs(i)
+//       a++
+//     }
+//   }
+//   return a
+// }
+function solution(n, c){
+    let a=0
+    let v=Array(n).fill(false)
+    function dfs(index){
+        v[index]=true
+        for(let i=0;i<n;i++){
+            if(c[index][i] && !v[i]){
+                dfs(i)
+            }
+        }
     }
-  }
-  for (let i = 0; i < n; i++) {
-    if (!v[i]) {
-      dfs(i)
-      a++
+    for (let i=0;i<n;i++){
+        if(!v[i]){
+            dfs(i)
+            a++
+        }
     }
-  }
-  return a
+    return a
 }
