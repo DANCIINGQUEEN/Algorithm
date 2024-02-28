87b2a53cb1e2fcd4bbfaefb91696d162b6209760
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-02-28 12:42:53 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,4 +1,4 @@
-const gcd=(a,b)=>a%b===0?b:gcd(b, a%b)
+const gcd=(a,b)=>a%b?gcd(b, a%b):b
 function solution(numer1, denom1, numer2, denom2) {
     let d=denom1*numer2+denom2*numer1
     let n=denom1*denom2
