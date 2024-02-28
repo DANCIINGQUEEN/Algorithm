f093dd505f29ba68540f2071e317bfff1d857896
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-02-28 15:41:56 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,9 +1,17 @@
-function solution(n) {
-    let b=new Set()
-    for(let i=0;i<n.length;i++){
-        for(let j=0;j<i;j++){
-            b.add(n[i]*n[j])
-        }
-    }
-    return [...b].sort((a,b)=>b-a)[0]
+// function solution(n) {
+//     let b=new Set()
+//     for(let i=0;i<n.length;i++){
+//         for(let j=0;j<i;j++){
+//             b.add(n[i]*n[j])
+//         }
+//     }
+//     return [...b].sort((a,b)=>b-a)[0]
+// } 시간복잡도 O(n^2)
+
+function solution(n){
+    n.sort((a,b)=>a-b)
+    let a=n.length
+    let x=n[a-1]*n[a-2]
+    let m=n[0]*n[1]
+    return Math.max(x, m)
 }
