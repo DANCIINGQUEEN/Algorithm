49d2156e7004bbd1d6fd95030b2aa66051e93c01
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-02 21:53:46 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,10 +1,10 @@
-function solution(m) {
-    let a=[]
-    for(let i of m){
-        if(!(a.indexOf(i)+1)) a.push(i)
-    }
-    return a.join``
-}
-// function solution(m){
-    
+// function solution(m) {
+//     let a=[]
+//     for(let i of m){
+//         if(!(a.indexOf(i)+1)) a.push(i)
+//     }
+//     return a.join``
 // }
+function solution(m){
+    return [...new Set(m)].join``
+}
