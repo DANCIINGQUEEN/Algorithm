0385e9f61a1ab31465999cd988286483aacacf93
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-02 14:57:07 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,10 +1,14 @@
-function solution(n, direction) {
-    if(direction==='right'){
-        let a=n.pop()
-        n=[a, ...n]
-    }else{
-        let a=n.shift()
-        n=[...n, a]
-    }
+// function solution(n, direction) {
+//     if(direction==='right'){
+//         let a=n.pop()
+//         n=[a, ...n]
+//     }else{
+//         let a=n.shift()
+//         n=[...n, a]
+//     }
+//     return n
+// }
+let solution=(n, d)=>{
+    d==='left'?n.push(n.shift()):n.unshift(n.pop())
     return n
 }
