a90b77b47d4fd0392adf2a5d7f20d1a4576bd0e2
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-02 15:56:37 +0900

    [level 0] Title: 369게임, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,11 +1,10 @@
-function solution(order) {
-    let a=0
-    order.toString().split``.forEach(x=>{
-        if([3,6,9].includes(+x)) {
-            console.log('yes', a)
-        a++
-            
-        }
-    })
-    return a
-}
+// function solution(order) {
+//     let a=0
+//     order.toString().split``.forEach(x=>{
+//         if([3,6,9].includes(+x)) {
+//         a++
+//         }
+//     })
+//     return a
+// }
+let solution=o=>Array.from(o.toString()).filter(x=>[3,6,9].includes(+x)).length
