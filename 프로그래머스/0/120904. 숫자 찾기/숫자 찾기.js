db3bad8db3abf669b5860e28ead0486c477e70e4
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-02 15:20:41 +0900

    [level 0] Title: 숫자 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,4 +1,5 @@
-function solution(num, k) {
-    let a=num.toString().split``.indexOf(k.toString())
-    return a===-1?-1:a+1
-}
+// function solution(num, k) {
+//     let a=num.toString().split``.indexOf(k.toString())
+//     return a===-1?-1:a+1
+// }
+let solution=(n,k)=>n.toString().split``.indexOf(k.toString())+1||-1
