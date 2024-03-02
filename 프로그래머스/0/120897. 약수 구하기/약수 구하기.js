6340c63a84d11f9bf26924d1455c6a68a3836137
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-02 14:50:56 +0900

    [level 0] Title: 약수 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -1,7 +1,8 @@
-function solution(n) {
-    let a=[]
-    for(let i=1;i<=n;i++){
-        if(!(n%i)) a.push(i)
-    }
-    return a
-}
+// function solution(n) {
+//     let a=[]
+//     for(let i=1;i<=n;i++){
+//         if(!(n%i)) a.push(i)
+//     }
+//     return a
+// }
+let solution=n=>Array.from({length:n},(_,i)=>i+1).filter(x=>!(n%x))
