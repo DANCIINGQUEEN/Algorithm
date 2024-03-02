ed353b1cb38b36f9b60670e1123990cb8e4beb69
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-02 15:05:42 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,5 +1,3 @@
 function solution(age) {
-    let n=['a','b','c','d','e','f','g','h','i','j']
-    // console.log([1,2,3][1])
-    return age.toString().split``.map(x=>n[x]).join``
+    return age.toString().split``.map(x=>'abcdefghij'[x]).join``
 }
