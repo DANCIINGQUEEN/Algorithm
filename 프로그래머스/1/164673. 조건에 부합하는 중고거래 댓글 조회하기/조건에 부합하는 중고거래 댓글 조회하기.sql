e05e8baefb55e6fe838e4f1abcbb6fac2de994b0
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-07 10:32:07 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,5 @@
+select b.title, b.board_id, r.reply_id, r.writer_id, r.contents, date_format(r.created_date,'%Y-%m-%d') created_date
+from used_goods_board b, used_goods_reply r
+where b.board_id=r.board_id and 
+    b.created_date like '2022-10-%'
+order by created_date, title
