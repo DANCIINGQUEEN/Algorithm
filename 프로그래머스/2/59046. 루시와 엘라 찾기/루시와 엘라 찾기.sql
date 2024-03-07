d28173c045f9a08f45f5bc30711007e78112052d
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-07 14:56:15 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,8 +1,10 @@
 select animal_id, name, sex_upon_intake
 from animal_ins
-where name='Lucy' or
-    name='Ella' or
-    name='Pickle' or    
-    name='Rogan' or
-    name='Sabrina' or    
-    name='Mitty'
+# where name='Lucy' or
+#     name='Ella' or
+#     name='Pickle' or    
+#     name='Rogan' or
+#     name='Sabrina' or    
+#     name='Mitty'
+where name in ('Lucy',"Ella","Pickle",'Rogan','Sabrina','Mitty')
+order by animal_id
