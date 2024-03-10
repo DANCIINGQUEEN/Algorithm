5f228fb87934670bb007e752c7c68fd5de343873
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-10 22:57:43 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.92 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,14 +1,10 @@
-// function solution(n, c) {
-//     let a=0
-//     let s={'w':1, 's':-1, 'd':10,'a':-10}
-//     c.split``.forEach(x=>{
-//         a+=s[x]
-//         console.log(x, a)
-        
-//     })
-//     return a
-// }
-function solution(n, control) {
-    return n + [...control].filter(v => v == "w").length - [...control].filter(v => v == "s").length
-        + 10 * [...control].filter(v => v == "d").length - (10 * [...control].filter(v => v == "a").length);
+let s = {
+  w: (n) => n + 1,
+  s: (n) => n - 1,
+  d: (n) => n + 10,
+  a: (n) => n - 10,
+};
+
+function solution(n, c) {
+  return [...c].reduce((a,b) => s[b](a), n);
 }
