fa1a1d17566c7b80eec6716ec8b4fc9ae44ad7c7
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-10 21:59:51 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -1,5 +1,6 @@
 let p=(a,b)=>+(a+''+b)>2*a*b?+(a+''+b):2*a*b
 
 function solution(a, b) {
-    return +(a+''+b)>2*a*b?+(a+''+b):2*a*b
+    // return +(a+''+b)>2*a*b?+(a+''+b):2*a*b
+    return Math.max(+(a+''+b),2*a*b)
 }
