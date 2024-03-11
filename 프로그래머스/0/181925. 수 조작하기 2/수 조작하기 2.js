c6c8bdcae125883e01e1bfd88fdecd4ee015e0bf
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-11 18:32:02 +0900

    [level 0] Title: 수 조작하기 2, Time: 8.77 ms, Memory: 41.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let a=[]
+    for(let i=0;i<n.length;i++){
+        if(n[i+1]-n[i]===1) a.push('w')
+        if(n[i+1]-n[i]===10) a.push('d')
+        if(n[i+1]-n[i]===-1) a.push('s')
+        if(n[i+1]-n[i]===-10) a.push('a')
+    }
+    return a.join``
+    // return n.map(x=>{
+    //     if(x===0) return w
+    // })
+}
