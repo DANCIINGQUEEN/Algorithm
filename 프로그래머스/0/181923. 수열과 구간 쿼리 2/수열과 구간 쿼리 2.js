7b2d09582a8be8db0a6f17674cff7c79f3602d31
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-15 13:28:47 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 10.18 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -0,0 +1,17 @@
+function solution(a, q) {
+    let r=[]
+    let w=[]
+    q.forEach(([s,e,k])=>{
+        for(let i=s;i<=e;i++) if(a[i]>k) w.push(a[i])
+        w.sort((a,b)=>a-b)
+        r.push(w.length?w[0]:-1)
+        w=[]
+    })
+    return r
+    // let r=[]
+    // q.forEach(([s,e,k])=>{
+    //     let w=a.slice(s,e+1).filter(x=>x>k)
+    //     console.log(w)
+    //     w.sort()
+    // })
+}
