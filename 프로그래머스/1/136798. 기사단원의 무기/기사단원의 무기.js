e62a5c9f35234bf95b6a5fc0f42bb08f216a7dc0
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-18 21:56:46 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.65 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,18 @@
+function solution(n, l, p) {
+    let a=0
+    for(let i=1;i<=n;i++){
+        let d=0
+        for(let j=1;j<=Math.sqrt(i);j++){
+            if(!(i%j)){
+                if(i/j===j) d++
+                else d+=2
+            }
+            if(d>l){
+                d=p
+                break
+            }
+        }
+        a+=d
+    }
+    return a
+}
