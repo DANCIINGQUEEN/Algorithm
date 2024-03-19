05c97d86445a8ab0922a15a50e240e980384783a
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-19 14:08:39 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,4 +1,4 @@
-function solution(N, s) {
+// function solution(N, s) {
     // let a=[]
     // let b=[s.length]
     // let c={}
@@ -12,12 +12,19 @@ function solution(N, s) {
     // for(let i in c) d.push([i, c[i]])
     // d=d.sort((a,b)=>b[1]-a[1])
     // return d.map(x=>+x[0])
-    let a=[]
-    for(let i=1;i<=N;i++){
-        let r=s.filter(x=>x>=i).length
-        let c=s.filter(x=>x===i).length
-        a.push([i,c/r])
+// }
+function solution(N, s) {
+    let c = new Array(N+1).fill(0)
+    s.forEach(x=>x<=N?c[x]++:0)
+    let nC=s.length
+    let f=[]
+    for(let i=1;i<=N;i++) {
+        if(nC===0) f.push([i,0]) 
+         else {
+            f.push([i,c[i]/nC])
+            nC-=c[i] 
+        }
     }
-    a.sort((a,b)=>b[1]-a[1])
-    return a.map(x=>x[0])
+    f.sort((a,b)=>b[1]-a[1])
+    return f.map(x=>x[0])
 }
