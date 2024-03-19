8751234fc4f83fa6bc1b87528aed213466f40dc9
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-19 19:32:52 +0900

    [level 2] Title: 할인 행사, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,35 +1,14 @@
-// function solution(want, number, discount) {
-//     let r=0
-//     discount.forEach((x,i)=>{
-//         let copy=[...discount].slice(i, i+10)
-//         if(copy.length<10) return r
-//         let f=0
-//         for(let j=0;i<want.length;j++){
-//             if([...copy].filter(e=>e===want[j]).length===number[j]) f++
-//             else break
-//         }
-//         if(f===want.length) r++
-//     })
-//     return r
-// }
 function solution(want, number, discount) {
-    let result = 0
-    discount.forEach((v,i)=>{
-
-     let copy=[...discount].slice(i,i+10)
-     if(copy.length<10)return result
-
-     let flag=0
-     for(let j=0;j<want.length;j++){
-      if([...copy].filter(el=>el==want[j]).length==number[j]) 
-       flag++
-
-      else break
-     }
-
-     if(flag==want.length)result++
-
-  })
-
-    return result
+    let r=0
+    discount.forEach((x,i)=>{
+        let copy=[...discount].slice(i, i+10)
+        if(copy.length<10) return r
+        let f=0
+        for(let j=0;j<want.length;j++){
+            if([...copy].filter(e=>e==want[j]).length==number[j]) f++
+            else break
+        }
+        if(f==want.length) r++
+    })
+    return r
 }
