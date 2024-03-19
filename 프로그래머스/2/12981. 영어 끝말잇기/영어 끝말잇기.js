09b3e419b76fa176d27fab6f677acf431b659cb2
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-19 18:49:39 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,15 +1,11 @@
 function solution(n, words) {
     for (let i = 1; i < words.length; i++) {
-        let curWord = words[i];
-        if (words.indexOf(curWord) !== i) {
-            return [i % n + 1, Math.floor(i / n) + 1];
-        }
+        let w = words[i]
+        if (words.indexOf(w) !== i)return [i % n + 1, Math.floor(i / n) + 1]
         
-        let lastLetterBeforeWord = words[i - 1].slice(-1);
-        let firstLetterCurWord = curWord.charAt(0);
-        if (lastLetterBeforeWord !== firstLetterCurWord) {
-            return [i % n + 1, Math.floor(i / n) + 1];
-        }
+        let l = words[i - 1].slice(-1)
+        let f = w.charAt(0)
+        if (l !== f) return [i % n + 1, Math.floor(i / n) + 1]
     }
     return [0, 0];
 }
