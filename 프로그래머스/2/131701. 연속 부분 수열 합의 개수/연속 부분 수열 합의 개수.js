2affad54da1e817d7d191df0414871e541013cb9
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-19 19:26:57 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 54.61 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,10 +1,13 @@
-function solution(elements) {
-   let a=[...elements, ...elements]
-   let s=new Set()
-   for(let i=0;i<elements.length;i++){
-       for(let j=0;j+i<a.length;j++){
-           s.add(a.slice(j, j+i).reduce((a,c)=>a+c,0))
-       }
-   }
+function solution(e) {
+    let a = [...e, ...e]
+    let s = new Set()
+    for (let i = 0; i < e.length; i++) {
+        let sum = 0
+        for (let j = 0; j < i; j++) sum += a[j]
+        for (let j = 0; j < e.length; j++) {
+            sum += a[j + i] - (j > 0 ? a[j - 1] : 0)
+            s.add(sum)
+        }
+    }
     return s.size
 }
