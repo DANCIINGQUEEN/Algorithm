33d1f60d0f9dd7783eed146b439c4f1ae00fba6a
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-19 14:21:27 +0900

    [level 0] Title: 옹알이 (1), Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,13 +1,11 @@
+
 function solution(b) {
-    let p=["aya", "ye", "woo", "ma"]
+    let p=["aya","ye","woo","ma"]
+    let pattern=new RegExp(p.join('|'),'gi')
     let a=0
-    for(let i in b){
-        let s=b[i]
-        for(let j in p){
-            if(b[i].includes(p[j])) s=s.replace(p[j],'X')
-        }
-        s=s.replace(/X/gi,'')
-        if(s.length===0) a++
-    }
+    b.forEach(s=>{
+        let result=s.replace(pattern,'X').replace(/X/g,'')
+        if(result.length===0) a++
+    })
     return a
 }
