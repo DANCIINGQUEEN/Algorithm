1f9dfb44893e2f1b8c6c557e117933910c094c9a
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-19 13:45:37 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,15 +1,23 @@
 function solution(N, s) {
+    // let a=[]
+    // let b=[s.length]
+    // let c={}
+    // for(let i=1;i<=N+1;i++){
+    //     a.push(s.filter(x=>x>i).length)
+    //     b.push(a[i-1])
+    //     c[i+'']=(b[i-1]-a[i-1])/b[i-1]
+    //     if(Object.keys(c).length>=N) break
+    // }
+    // let d=[]
+    // for(let i in c) d.push([i, c[i]])
+    // d=d.sort((a,b)=>b[1]-a[1])
+    // return d.map(x=>+x[0])
     let a=[]
-    let b=[s.length]
-    let c={}
-    for(let i=1;i<=N+1;i++){
-        a.push(s.filter(x=>x>i).length)
-        b.push(a[i-1])
-        c[i+'']=(b[i-1]-a[i-1])/b[i-1]
-        if(Object.keys(c).length>=N) break
+    for(let i=1;i<=N;i++){
+        let r=s.filter(x=>x>=i).length
+        let c=s.filter(x=>x===i).length
+        a.push([i,c/r])
     }
-    let d=[]
-    for(let i in c) d.push([i, c[i]])
-    d=d.sort((a,b)=>b[1]-a[1])
-    return d.map(x=>+x[0])
+    a.sort((a,b)=>b[1]-a[1])
+    return a.map(x=>x[0])
 }
