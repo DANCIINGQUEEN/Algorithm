3b9e2eb410327ff9b17eac486ceab6eb844b8ef7
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-19 22:01:20 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,21 +1,37 @@
+// function solution(p, s) {
+//     let a=[]
+//     let r=[]
+//     for(let i=0;i<p.length;i++){
+//         a.push(Math.ceil((100-p[i])/s[i]))
+//     }
+//     while(a.length>0){
+//         while(a[0]>0){
+//             for(let i in a){
+//                 a[i]--
+//             }        
+//         }
+//         let i=0
+//         while(a[0]<=0){
+//             i++
+//             a.shift()
+//         }
+//         r.push(i)
+//     }
+//     return r
+// }
 function solution(p, s) {
-    let a=[]
-    let r=[]
-    for(let i=0;i<p.length;i++){
-        a.push(Math.ceil((100-p[i])/s[i]))
-    }
-    while(a.length>0){
-        while(a[0]>0){
-            for(let i in a){
-                a[i]--
-            }        
+    let d = p.map((x, i) => Math.ceil((100 - x) / s[i]))
+    let m = d[0]
+    let c = 1
+    let r = []
+    for (let i = 1; i < d.length; i++) {
+        if (d[i] <= m) c++
+        else {
+            r.push(c)
+            c = 1
+            m = d[i]
         }
-        let i=0
-        while(a[0]<=0){
-            i++
-            a.shift()
-        }
-        r.push(i)
     }
+    r.push(c)
     return r
 }
