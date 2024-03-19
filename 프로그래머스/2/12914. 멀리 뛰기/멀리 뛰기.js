f44315c88a41641eb909ae944a6c54dc7e632d13
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-19 19:22:11 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,7 +1,5 @@
-const solution = (n) => {
-  const dp = [0, 1, 2];
-  for(let i=3; i<=n; i++){
-    dp[i] = (dp[i-1] + dp[i-2])%1234567;
-  }
-  return dp[n];
+function solution(n) {
+    let dp=[0,1,2]
+    for(let i=3;i<=n;i++) dp[i]=(dp[i-1]+dp[i-2])%1234567
+    return dp[n]
 }
