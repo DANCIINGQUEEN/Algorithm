37d7759b4a916edc19db82735941e9611cb0258d
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-19 18:09:14 +0900

    [level 2] Title: 숫자의 표현, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,24 +1,8 @@
-// function solution(n) {
-//     let c=0
-//     for(let i=1;i<=n;i++){
-//         let s=0
-//         for(j=i;j<=n;j++){
-//             s+=j
-//             if(s===n) {
-//                 // console.log(j,s)
-//                 c++
-//                 continue
-                
-//             }
-//         }
-//     }
-//     return c;
-// }
 function solution(n) {
-    var answer = 0;
-    for (let i  =0; i <=n; i++) {
-        if ( n % i === 0 && i % 2 === 1 )   //약수이면서 홀수인 경우 
-            answer ++
+    var answer=0
+    for (let i=0;i<=n;i++){
+        if(n%i===0&&i%2===1)
+            answer++
     }
-    return answer;
+    return answer
 }
