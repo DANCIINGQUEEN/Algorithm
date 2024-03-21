27e7da6c1c5d6931041cb947489e3c3be7c05327
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-21 09:01:56 +0900

    [Bronze III] Title: ACM 호텔, Time: 140 ms, Memory: 9560 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.js
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.js
@@ -1,18 +1,7 @@
-let [q,...w]=require('fs').readFileSync('dev/stdin').toString().trim().split`\n`
-q=+q
+let [_,...w]=require('fs').readFileSync('dev/stdin').toString().trim().split`\n`
 w.forEach(x=>{
-    [h,w,e]=x.split` `.map(x=>+x)
-    let a=Array(h).fill(0).map((x,i)=>Array.from({length:w},(x,j)=>`${i+1}${(j+1)<10?'0'+(j+1):j+1}`))
-    let dh=0
-    let dw=0
-    let b
-    for(let i=0;i<e;i++){
-        b=a[dh][dw]
-        if(dh===h-1){
-            dh=0
-            dw++
-        }
-        else dh++
-    }
-    console.log(b)
+    let [h,_,e]=x.split` `.map(x=>+x)
+    let dh=((e-1)%h)+1
+    let dw=Math.ceil(e/h)
+    console.log(`${dh}${dw<10?'0'+dw:dw}`)
 })
