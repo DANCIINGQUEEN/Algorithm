050076791bc4cc1bffd65ad686c1a56c5e08a3a5
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-21 13:44:03 +0900

    [Silver IV] Title: 수 찾기, Time: 352 ms, Memory: 48000 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -1,24 +1,20 @@
+function bs(a,t){
+    let s=0
+    let e=a.length-1
+
+    while(s<=e){
+        let m=parseInt((s+e)/2)
+        if(t<a[m]) e=m-1
+        else if(t>a[m]) s=m+1
+        else return true
+    }
+    return false
+}
+
 let i=require('fs').readFileSync('dev/stdin').toString().trim().split`\n`
 let n=i[1].split` `.map(v=>+v)
 let m=i[3].split` `.map(v=>+v)
-
 n.sort((a,b)=>a-b)
 let r=[]
-
-m.forEach(x=>{
-    let s=0
-    let e=n.length-1
-    let is=false
-    while(s<=e){
-        let mid=parseInt((s+e)/2)
-        if(x<n[mid]) e=mid-1
-        else if(x>n[mid]) s=mid+1
-        else{ 
-            is=true
-            break  
-        }
-    }
-    if(is) r.push(1)   
-    else r.push(0)  
-})
+m.forEach(x=>{r.push(bs(n,x)?1:0)})
 console.log(r.join`\n`)
