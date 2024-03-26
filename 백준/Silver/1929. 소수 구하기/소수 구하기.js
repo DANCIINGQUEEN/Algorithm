b6e272958146908fc8435f0563d20bf511e56967
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-26 23:05:19 +0900

    [Silver III] Title: 소수 구하기, Time: 232 ms, Memory: 24236 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.js
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -1,17 +1,25 @@
-let [a,b]=require('fs').readFileSync('dev/stdin').toString().trim().split(' ').map(e=>+e)
-function p(a,b){
-    let ip=new Array(b+1).fill(true)
-    ip[0]=false
-    ip[1]=false
-    for(let i=2;i*i<=b;i++){
-        if(ip[i]){
-            for(let j=i*i;j<=b;j+=i){
-                ip[j]=false
+let [a, b] = require('fs').readFileSync('dev/stdin').toString().trim().split(' ').map(e => +e);
+function p(a, b) {
+    let ip = new Array(b + 1).fill(true);
+    ip[0] = ip[1] = false;
+    let output = [];
+    
+    for (let i = 2; i * i <= b; i++) {
+        if (ip[i]) {
+            // 최적화된 시작점 계산
+            let start = Math.max(i * i, Math.ceil(a / i) * i);
+            for (let j = start; j <= b; j += i) {
+                ip[j] = false;
             }
         }
     }
-    for(let i=a;i<=b;i++){
-        if(ip[i]) console.log(i)
+    
+    for (let i = a; i <= b; i++) {
+        if (ip[i]) output.push(i);
     }
+
+    // 한 번에 모든 소수 출력
+    console.log(output.join('\n'));
 }
-p(a,b)
+
+p(a, b);
