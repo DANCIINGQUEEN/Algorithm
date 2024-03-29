c4b915b3e82ac4a308d7c7207d2fce72c12874f7
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-03-29 22:14:51 +0900

    [Bronze II] Title: Hashing, Time: 136 ms, Memory: 9584 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -1,3 +1,14 @@
-let [L,W]=require('fs').readFileSync('dev/stdin').toString().trim().split`\n`
-let a='abcdefghijklmnopqrstuvwxyz'
-console.log([...W].reduce((x,c,i)=>x+(a.indexOf(c)+1)*31**i,0))
+const fs = require('fs');
+const [n, str] = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
+
+const N = +n
+let hash = 0;
+let r = 1;
+for (let i = 0; i < N; i++) {
+  hash += (str.charCodeAt(i) - 96) * r
+  hash %= 1234567891;
+  r *= 31
+  r %= 1234567891;
+}
+
+console.log(hash)
