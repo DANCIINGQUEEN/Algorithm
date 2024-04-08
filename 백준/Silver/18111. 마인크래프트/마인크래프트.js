f631734e15a9e7773e919f5855e521e27405a4a4
park <77094412+DANCIINGQUEEN@users.noreply.github.com>
2024-04-08 17:56:14 +0900

    [Silver II] Title: 마인크래프트, Time: 228 ms, Memory: 16448 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.js
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.js
@@ -1,24 +1,35 @@
-let input = require("fs").readFileSync("dev/stdin").toString().trim().split`\n`
-let [n, m, b] = input.shift().split` `.map(Number)
-let a = input.map((x) => x.split` `.map(Number))
-let t = Infinity
-let r = -1
+let input = require("fs").readFileSync("dev/stdin").toString().trim().split`\n`;
+let [n, m, b] = input.shift().split` `.map(Number);
+let a = input.map((x) => x.split` `.map(Number));
+
+let heightCount = new Array(257).fill(0); 
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < m; j++) {
+    heightCount[a[i][j]]++;
+  }
+}
+
+let t = Infinity;
+let r = -1;
 for (let h = 0; h <= 256; h++) {
-  let v = 0
-  let c = 0
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      let curH = a[i][j] - h
-      if (curH < 0) v -= curH
-      else c += curH
+  let v = 0; 
+  let c = 0; 
+
+  for (let i = 0; i <= 256; i++) {
+    if (i < h) {
+      v += (h - i) * heightCount[i];
+    } else if (i > h) {
+      c += (i - h) * heightCount[i];
     }
   }
+
   if (c + b >= v) {
-    let e = 2 * c + v
+    let e = 2 * c + v;
     if (t >= e) {
-      t = e
-      r = h
+      t = e;
+      r = h;
     }
   }
 }
-console.log(t, r)
+console.log(t, r);
